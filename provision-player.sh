c4e502609e060f0ae509c8a14d53ff1cd66fd10e
greg <gstencel@gmail.com>
2019-10-20 20:21:07 +0100

    Working cluster

--- provision-player.sh
+++ provision-player.sh
@@ -1,6 +1,8 @@
 USER=vagrant
 SYNC_FOLDER='/vagrant'
 echo "bootstrap start"
+export DEBIAN_FRONTEND=noninteractive
+
 set -x
 set -e # exit on error
 cat $SYNC_FOLDER/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
@@ -12,12 +14,12 @@ chmod 600 /home/vagrant/.ssh/id_rsa
 echo "---------------------------------------------"
 echo "UPDATING atp repo"
 echo "---------------------------------------------"
-sudo apt-get -y update
+sudo apt-get -yq update
 echo "---------------------------------------------"
 echo "INSTALLING sys packages"
 echo "---------------------------------------------"
 #sudo apt-get install -y $(cat $SYNC_FOLDER/system-requirements.txt)
-sudo apt-get -y install ansible 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install ansible 
 
 echo "Installing k8s"
 su - vagrant -c "ansible-playbook -i /vagrant/hosts /vagrant/1_k8s_install.yml"
